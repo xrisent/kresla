bd4b120dd7e0eb3e8066add03663c8912f436650
xrisent <ormonov31261@gmail.com>
2026-05-26 20:29:44 +0600

    header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,11 +1,21 @@
-import "./Header.css";
-
 function Header() {
   return (
     <header>
-      <div className="container">
-        <div className="header-right">
-          <nav>
+      <div className="container flex flex-col py-[17px] gap-[16px]">
+        <div className="flex justify-between items-center">
+          <img src="/Logo.png" className="w-[184px] h-[42px]" />
+          <input
+            type="text"
+            placeholder="Название модели или артикул"
+            className="w-127.5 h-12.75 bg-[#f7f7f8] py-[15px] px-[20px]"
+          />
+          <p>+7 495 221-06-75</p>
+          <button className="px-[28px] py-[15px] bg-[#f3f4f7]">
+            Заказать звонок
+          </button>
+        </div>
+        <div>
+          <nav className="flex gap-[34px]">
             <a href="/">Главная</a>
             <a href="/about">О продукте</a>
             <a href="">Отзывы</a>
