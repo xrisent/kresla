707a5775daecd78db90dc56f65d93f46218aa25d
xrisent <ormonov31261@gmail.com>
2026-06-06 20:40:08 +0600

    pagination

--- src/pages/Home/Catalog/Catalog.jsx
+++ src/pages/Home/Catalog/Catalog.jsx
@@ -1,17 +1,72 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 
 export const Catalog = () => {
-  fetch("/api/catalog", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ name: "John" }),
-  })
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+  const [totalData, setTotalData] = useState([]);
+  const [data, setData] = useState();
+  const [page, setPage] = useState(1);
 
-  axios
-    .post("/api/catalog", { name: "John" })
-    .then((response) => console.log(response));
+  const totalPage = Math.ceil(totalData.length / 10);
 
-  return <div className="container"></div>;
+  const handleNext = () => {
+    if(page === totalPage)return;
+    setPage((prev) => prev + 1);
+  };
+
+  const handlePrev = ()=>{
+    if(page === 1)return;
+    setPage((prev)=>prev-1)
+  }
+
+  useEffect(() => {
+    axios("http://localhost:3000/armchairs").then(({ data }) =>
+      setTotalData(data),
+    );
+  }, []);
+
+  useEffect(() => {
+    const fetchCatalog = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/armchairs", {
+          // queryParams - это параметры запроса, которые мы передаем бэку. допустим в данном случае мы говорим бэку что нам нужно первая страница и 10 элементов
+          params: { _page: page, _per_page: 10 },
+          // пагинация - позволяет разбивать большой объем данных на отдельные, последовательные страницы. к примеру 100 объектов и мы делим его на 10 частей по 10 объектов
+          // позволяет облегчать загрузку сайта и запроса бэку
+        });
+
+        setData(response.data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchCatalog();
+  }, [page]);
+
+  return (
+    <div className="container pb-20">
+      <h1 className="text-center font-bold">Каталог кресел</h1>
+      <div className="container  pt-[100px] pb-[50px] flex flex-wrap text-center gap-4.5">
+        {data &&
+          data.map((el) => (
+            <div className="cards">
+              <img className="w-[278px] h-[333px ]" src="/creslo.png" alt="" />
+              <p className="w-[273px]">{el.title}</p>
+              <p className="text-2xl font-bold">{el.price}</p>
+            </div>
+          ))}
+      </div>
+      <div className="flex gap-10">
+        <button onClick={handlePrev}>назад</button>
+        <ul className="flex gap-5">
+          {Array.from({ length: totalPage }).map((item, idx) => (
+            <li className={`text-2xl w-10 h-10 border flex items-center justify-center ${(idx+1) === page?"border-red-600":""}` }>
+              {idx + 1}
+            </li>
+          ))}
+        </ul>
+        <button onClick={handleNext}>вперед</button>
+      </div>
+    </div>
+  );
 };
